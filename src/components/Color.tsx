ae12e93fc5509f3f4d68b1f5c0c929ac39173c65
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-08 18:45:41 -0300

    style(Color): round component

--- src/components/Color.tsx
+++ src/components/Color.tsx
@@ -5,5 +5,5 @@ interface ColorProps {
 }
 
 export default function Color({ bgColor }: ColorProps) {
-  return <div className="w-12 h-12" style={{ backgroundColor: bgColor }} />;
+  return <div className="w-12 h-12 rounded-full" style={{ backgroundColor: bgColor }} />;
 }
