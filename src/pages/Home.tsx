0b20b36f7949aa54e56f1d95be7bdfdb21c9dd9b
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-08 18:40:28 -0300

    feat(Home): import and render Pallete

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import Pallete from '../components/Pallete'
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Pallete />
     </>
   )
 }
