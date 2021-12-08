913509443a3c80e7cd8b3919d17c55b4f73e693d
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-08 20:02:13 -0300

    build(tailwind): customize border width

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   theme: {
+    borderWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '1': '1px',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '8px',
+    },
     extend: {},
   },
   variants: {
