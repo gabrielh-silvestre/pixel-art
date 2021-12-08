8ac6385b1e3ec2e7ec2c24a9270ffbaa96f14343
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-08 18:12:16 -0300

    feat(components): create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className="w-full py-4 bg-green-700">
+      <div className="w-4/5 mx-auto flex justify-between items-center">
+        <h1 className="text-3xl">Pixel Art</h1>
+
+        <nav className="text-lg">
+          <a
+            href="https://github.com/gabrielh-silvestre"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mr-6"
+          >
+            GitHub
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/gabrielh-silvestre/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Linkedin
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+}
