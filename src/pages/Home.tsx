3a40402777ab6dcbdcfa90afc3da386bf88fb36e
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-08 20:08:38 -0300

    feat(Home): import and render Canvas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import Header from '../components/Header'
-import Pallete from '../components/Pallete'
+import React from 'react';
+
+import Header from '../components/Header';
+import Pallete from '../components/Pallete';
+import Canvas from '../components/Canvas';
 
 export default function Home() {
   return (
-    <>
+    <div>
       <Header />
-      <Pallete />
-    </>
-  )
+      <main className="w-4/5 mx-auto mt-12 flex justify-between">
+        <Pallete />
+        <Canvas />
+      </main>
+    </div>
+  );
 }
