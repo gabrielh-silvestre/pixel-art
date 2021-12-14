d726ec325c106279eb0c63e3169323fc51703320
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-14 11:19:50 -0300

    feat(Home): encapsulate Pallete and Canvas with ColorProvider

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-
+import { ColorProvider } from '../hooks/useColor';
 import Header from '../components/Header';
 import Pallete from '../components/Pallete';
 import Canvas from '../components/Canvas';
@@ -8,10 +7,12 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <main className="w-4/5 mx-auto mt-12 flex justify-between">
-        <Pallete />
-        <Canvas />
-      </main>
+      <ColorProvider>
+        <main className="w-4/5 mx-auto mt-12 flex justify-between">
+          <Pallete />
+          <Canvas />
+        </main>
+      </ColorProvider>
     </div>
   );
 }
