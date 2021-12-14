cfa179dfadb7d2699c8736f023580c75b1ed808f
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-14 11:20:46 -0300

    feat(Pixel): paint pixel based on selected color

--- src/components/Pixel.tsx
+++ src/components/Pixel.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useColor } from '../hooks/useColor';
 
 interface PixelProps {
   size: number;
 }
 
 export default function Pixel({ size }: PixelProps) {
+  const { selectedColor } = useColor();
+  const [thisColor, setThisColor] = useState('white');
+
   return (
     <div
       className="border-1 border-black border-solid inline-block"
-      style={{ width: `${384 / size}px`, height: `${384 / size}px`, backgroundColor: 'white' }}
+      style={{
+        width: `${384 / size}px`,
+        height: `${384 / size}px`,
+        backgroundColor: thisColor,
+      }}
+      onClick={() => {
+        setThisColor(selectedColor);
+      }}
     />
   );
 }
