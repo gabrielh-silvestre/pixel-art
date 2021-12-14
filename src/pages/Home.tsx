bac6c7a1f4ca463d03653c50d10a569a0929e0ee
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-14 14:46:06 -0300

    feat(Home): import and render Eraser

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ import Pallete from '../components/Pallete';
 import PalleteSettings from '../components/PalleteSettings';
 import Canvas from '../components/Canvas';
 import CanvasSettings from '../components/CanvasSettings';
+import Eraser from '../components/Eraser';
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
         <main className="w-4/5 mx-auto mt-12 flex justify-between">
           <div className="h-full flex flex-col justify-around items-center">
             <Pallete />
+            <Eraser />
             <PalleteSettings />
             <CanvasSettings />
           </div>
