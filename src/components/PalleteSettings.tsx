71fe20bffe8e5113e6db4cd0ac511f54402f1e12
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-14 12:42:32 -0300

    style(PalleteSettings): customize label input and button

--- src/components/PalleteSettings.tsx
+++ src/components/PalleteSettings.tsx
@@ -10,18 +10,27 @@ export default function PalleteSettings() {
   };
 
   return (
-    <div>
-      <label htmlFor="many-colors-input" className="flex flex-col">
+    <div className="flex items-end">
+      <label
+        htmlFor="many-colors-input"
+        className="mt-6 flex flex-col text-gray-200"
+      >
         Quantas cores?
         <input
           type="number"
           id="many-colors-input"
+          className="h-8 pl-4 rounded-md text-black"
           onChange={({ target: { value } }) => {
             setManyColors(Number.parseInt(value, 10));
           }}
         />
       </label>
-      <button type="button" onClick={handleNewColorQuantity}>
+
+      <button
+        type="button"
+        className="ml-2 py-1 px-3 bg-green-800 rounded-xl"
+        onClick={handleNewColorQuantity}
+      >
         Gerar
       </button>
     </div>
