d492cda900dfacab148ed3f49ac130486e9b4a1e
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-14 11:20:29 -0300

    feat(Color): set selected color

--- src/components/Color.tsx
+++ src/components/Color.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import { useColor } from '../hooks/useColor';
 
 interface ColorProps {
   bgColor: string;
 }
 
 export default function Color({ bgColor }: ColorProps) {
+  const { setSelectedColor } = useColor();
+
   return (
     <div
       className="w-12 h-12 mx-1 rounded-full shadow"
       style={{ backgroundColor: bgColor }}
+      onClick={() => setSelectedColor(bgColor)}
     />
   );
 }
