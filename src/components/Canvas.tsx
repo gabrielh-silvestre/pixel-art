95627a9c02b1b1a23f276c1993d0722e17e1784b
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-15 13:35:54 -0300

    feat(Canvas): implement reset button

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -8,6 +8,7 @@ import Lines from './Lines';
 export default function Canvas() {
   const { size } = useArt();
   const [lines, setLines] = useState<JSX.Element[]>([]);
+  const [resetCanvasKey, setResetCanvasKey] = useState(0);
 
   const genLines = () => {
     setLines([]);
@@ -19,19 +20,25 @@ export default function Canvas() {
     }
   };
 
+  const genRandomKey = () => Math.round(Math.random() * 100);
+
+  const resetCanvas = () => {
+    setResetCanvasKey(genRandomKey());
+  }
+
   useEffect(() => {
     genLines();
   }, [size]);
 
   return (
     <div className="flex-col items-center">
-      <section className="canvas w-96 h-96 rounded-xl">
+      <section key={resetCanvasKey} className="canvas w-96 h-96 rounded-xl">
         {lines.map((line) => line)}
       </section>
       <button
         type="button"
         className="w-full mt-4 py-1 px-3 bg-gray-200 rounded-md"
-        onClick={genLines}
+        onClick={resetCanvas}
       >
         Reset
       </button>
