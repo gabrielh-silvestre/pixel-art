5732811257ea5cdf32e0cda0adb1ddf975afec86
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-16 13:08:07 -0300

    style(Eraser): add hover effect

--- src/components/Eraser/index.tsx
+++ src/components/Eraser/index.tsx
@@ -6,7 +6,7 @@ export default function Eraser() {
   return (
     <button
       type="button"
-      className="w-32 h-10 mt-8 bg-gray-light rounded-md"
+      className="w-32 h-10 mt-8 bg-gray-light rounded-md hover:opacity-90 transition-opacity duration-200"
       onClick={() => {
         setSelectedColor('white');
       }}
