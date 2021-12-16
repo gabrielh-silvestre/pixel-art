22a9e0156308f976f0ddbbfab1cd008febcf96ad
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-16 12:44:20 -0300

    style(Home): change inline classNames for tw styled component

---
+++ src/pages/Home/style.ts
@@ -0,0 +1,18 @@
+import tw from 'tailwind-styled-components';
+
+export const HomeMain = tw.main`
+  max-w-screen-xl
+  mx-auto
+  mt-12
+  px-12
+  flex
+  justify-between
+`;
+
+export const ContentContainer = tw.div`
+  h-full
+  flex
+  flex-col
+  justify-around
+  items-center
+`;
