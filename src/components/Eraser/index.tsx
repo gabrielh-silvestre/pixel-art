fa262d39edde4f14bb88ef4eabb2664811405275
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-16 13:39:10 -0300

    style(Eraser): change inline classNames for tw styled component

--- src/components/Eraser/index.tsx
+++ src/components/Eraser/index.tsx
@@ -1,17 +1,18 @@
 import { useArt } from '../../hooks/useArt';
 
+import { EraserButton } from './style';
+
 export default function Eraser() {
   const { setSelectedColor } = useArt();
 
   return (
-    <button
+    <EraserButton
       type="button"
-      className="w-32 h-10 mt-8 bg-gray-light rounded-md hover:opacity-90 transition-opacity duration-200"
       onClick={() => {
         setSelectedColor('white');
       }}
     >
       Borracha
-    </button>
+    </EraserButton>
   );
 }
