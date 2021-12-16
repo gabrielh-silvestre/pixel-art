5eb053de9fa9045750d1dc8ae76f9c994e58d74c
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-16 12:05:04 -0300

    style(Canvas): change inline classNames for tw styled component

---
+++ src/components/Canvas/style.ts
@@ -0,0 +1,22 @@
+import tw from 'tailwind-styled-components';
+
+export const Container = tw.section`
+  flex-col
+  items-center
+`;
+
+export const CanvasContainer = tw.div`
+  canvas
+  w-96
+  h-96
+  rounded-xl
+`;
+
+export const Button = tw.button`
+  w-full
+  mt-4
+  py-1
+  px-3
+  bg-gray-light
+  rounded-md
+`;
