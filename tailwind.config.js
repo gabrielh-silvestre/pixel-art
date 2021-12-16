1237cdec19a8c06c76a09f2b9927450f8062b7af
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-16 11:33:21 -0300

    build(Tailwind): change color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
       '6': '6px',
       '8': '8px',
     },
+    colors: {
+      'green-dark': '#036b52',
+      'purple-dark': '#41197f',
+      'gray-light': '#e1e5eb',
+      'gray-dark': '#1a1b1c',
+    },
     extend: {},
   },
   variants: {
