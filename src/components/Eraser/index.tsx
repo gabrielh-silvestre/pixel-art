6edb5407172227351ccf2f705882b4187dc255f4
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-17 11:13:16 -0300

    style(Eraser): add ring if eraser is selected

--- src/components/Eraser/index.tsx
+++ src/components/Eraser/index.tsx
@@ -3,11 +3,12 @@ import { useArt } from '../../hooks/useArt';
 import { EraserButton } from './style';
 
 export default function Eraser() {
-  const { setSelectedColor } = useArt();
+  const { setSelectedColor, selectedColor } = useArt();
 
   return (
     <EraserButton
       type="button"
+      $isSelected={selectedColor === 'white'}
       onClick={() => {
         setSelectedColor('white');
       }}
