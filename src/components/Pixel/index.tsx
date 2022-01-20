45f8b4859c944d27dca6371cae674651776ceb15
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-01-20 18:15:33 -0300

    refactor(components/Pixel): add internal key
    - Reset pixel when resetCanvas hook state change

--- src/components/Pixel/index.tsx
+++ src/components/Pixel/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { useArt } from '../../hooks/useArt';
 
@@ -6,25 +6,33 @@ import { PixelContainer } from './style';
 
 interface PixelProps {
   size: number;
+  index: number;
 }
 
-export default function Pixel({ size }: PixelProps) {
-  const { selectedColor, dragPaint, setDragPaint } = useArt();
+export default function Pixel({ size, index }: PixelProps) {
+  const { selectedColor, dragPaint, resetCanvas, setDragPaint } = useArt();
   const [thisColor, setThisColor] = useState('white');
 
   const handleDragPaint = () => {
     dragPaint && setThisColor(selectedColor);
   };
 
+  const handleStartPaint = () => {
+    setThisColor(selectedColor);
+    setDragPaint(true);
+  };
+
+  useEffect(() => {
+    setThisColor('white');
+  }, [resetCanvas])
+
   return (
     <PixelContainer
+      key={`${thisColor}+${index}`}
       size={size}
       bgColor={thisColor}
       onMouseUp={() => setDragPaint(false)}
-      onMouseDown={() => {
-        setThisColor(selectedColor);
-        setDragPaint(true);
-      }}
+      onMouseDown={handleStartPaint}
       onMouseOver={handleDragPaint}
       onDragStart={() => false}
     />
