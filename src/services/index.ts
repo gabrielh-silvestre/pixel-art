bc26ae87a12f72f1acf2d5cf20ff4b0cac4a9409
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-01-21 12:26:30 -0300

    feat(services): create function to prevent drag

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,11 @@
+import React from "react";
+
 const getRandomNumber = (): number => Math.floor(Math.random() * 255);
 
 export const getRandomColor = (): string => {
   return `rgb(${getRandomNumber()}, ${getRandomNumber()}, ${getRandomNumber()})`;
 };
+
+export const dragPreventDefault = (event: React.DragEvent) => {
+  event.preventDefault();
+}
