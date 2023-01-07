5d6421d5d114995150dd06a352a8984ceaa3dc75
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2023-01-07 18:05:59 -0300

    feat(vite.config): set up vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    include: [
+      "src/**/*.test.ts",
+      "src/**/*.test.tsx",
+      "src/**/*.spec.ts",
+      "src/**/*.spec.tsx",
+    ],
+    exclude: ["./src/**/*.interface.ts", "./src/**/*.dto.ts"],
+    mockReset: true,
+  },
 });
