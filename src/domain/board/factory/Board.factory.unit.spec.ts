1813b6361ffa0e6bcfeefe199c57456cf0f83d48
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2023-01-08 00:52:21 -0300

    test(domain/board/factory): cover createFrom case

--- src/domain/board/factory/Board.factory.unit.spec.ts
+++ src/domain/board/factory/Board.factory.unit.spec.ts
@@ -5,6 +5,8 @@ import { BoardFactory } from "./Board.factory";
 
 import { HashMap } from "@domain/shared/hash-map/HashMap";
 
+import { BOARDS } from "@mocks/boards.mock";
+
 describe("Unit test for BoardFactory", () => {
   it("should create a new Board successfully", () => {
     const board = BoardFactory.create("Board 1", 5);
@@ -14,5 +16,17 @@ describe("Unit test for BoardFactory", () => {
 
     expect(board.title).to.be.eq("Board 1");
     expect(board.pixels.size).to.be.eq(25);
+  });
+
+  it("should create a new Board from an existing one successfully", () => {
+    const [board] = BOARDS;
+
+    const newBoard = BoardFactory.createFrom(board);
+
+    expect(newBoard).to.be.instanceOf(Board);
+    expect(newBoard.pixels).to.be.instanceOf(HashMap);
+
+    expect(newBoard.title).to.be.eq("Board 1");
+    expect(newBoard.pixels.size).to.be.eq(25);
   });
 });
