7d59bb9ec48946fc047968a1fe7a233856adefc1
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2023-01-08 03:31:29 -0300

    fix(infra/adapter/database): create recursive normalizer for localStorage
    - create internal recover for other operations

--- src/infra/adapter/database/board/localStorage/Database.localStorage.adapter.ts
+++ src/infra/adapter/database/board/localStorage/Database.localStorage.adapter.ts
@@ -8,18 +8,22 @@ export class BoardDatabaseLocalStorageAdapter<T>
 {
   constructor(private readonly key: string) {}
 
-  async getOne(dto: Partial<T>): Promise<T | null> {
-    const items = await this.getAll();
-    if (items.length === 0) return null;
+  private recursiveNormalizePropertyNames(data: any): any {
+    if (Array.isArray(data)) {
+      return data.map((item) => this.recursiveNormalizePropertyNames(item));
+    }
 
-    const item = items.find((item: any) =>
-      Object.entries(dto).every(([key, value]) => item[`_${key}`] === value)
-    );
+    if (typeof data === "object") {
+      return Object.entries(data).reduce((acc, [key, value]) => {
+        const newKey = key.replace(/^_/, "");
+        return { ...acc, [newKey]: this.recursiveNormalizePropertyNames(value) };
+      }, {});
+    }
 
-    return item || null;
+    return data;
   }
 
-  async getAll(): Promise<T[]> {
+  private internalRecover(): T[] {
     const items = localStorage.getItem(this.key);
 
     if (!items) return [];
@@ -27,15 +31,34 @@ export class BoardDatabaseLocalStorageAdapter<T>
     return JSON.parse(items);
   }
 
+  async getOne(dto: Partial<T>): Promise<T | null> {
+    const items = this.internalRecover();
+    if (items.length === 0) return null;
+
+    const item = items.find((item: any) =>
+      Object.entries(dto).every(([key, value]) => item[`_${key}`] === value)
+    );
+
+    return item ? this.recursiveNormalizePropertyNames(item) : null;
+  }
+
+  async getAll(): Promise<T[]> {
+    const items = localStorage.getItem(this.key);
+
+    if (!items) return [];
+
+    return JSON.parse(items).map(this.recursiveNormalizePropertyNames);
+  }
+
   async save(data: T): Promise<void> {
-    const items = await this.getAll();
+    const items = this.internalRecover();
     const newItems = [...items, data];
 
     localStorage.setItem(this.key, JSON.stringify(newItems));
   }
 
   async update(id: string, data: T): Promise<void> {
-    const items = await this.getAll();
+    const items = this.internalRecover();
 
     const newItems = items.map((item: any) => {
       if (item.id === id) return { ...item, ...data };
@@ -46,7 +69,7 @@ export class BoardDatabaseLocalStorageAdapter<T>
   }
 
   async delete(id: string): Promise<void> {
-    const items = await this.getAll();
+    const items = this.internalRecover();
 
     const newItems = items.filter((item: any) => item.id !== id);
 
